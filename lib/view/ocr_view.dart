b3e93835ed4e43f6bd530d5d4f6cd632ccdbd05f
Daniel Adel <168005559+DanielAdel-2107@users.noreply.github.com>
2024-06-08 17:16:06 +0300

    first commit

---
+++ lib/view/ocr_view.dart
@@ -0,0 +1,209 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:bldapp/view/DonationView.dart';
+import 'package:bldapp/view/chat_bot.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+class OCR_View extends StatefulWidget {
+  const OCR_View({super.key});
+
+  @override
+  _OCR_ViewState createState() => _OCR_ViewState();
+}
+
+class _OCR_ViewState extends State<OCR_View>
+    with SingleTickerProviderStateMixin {
+  String? _result;
+  final picker = ImagePicker();
+
+  Future<void> _uploadImage() async {
+    if (imageFile == null) {
+      setState(() {
+        _result = 'Please select an image first';
+      });
+      return;
+    }
+
+    var url = Uri.parse('https://blood-ocr.onrender.com');
+    var request = http.MultipartRequest('POST', url)
+      ..files.add(await http.MultipartFile.fromPath('image', imageFile!.path));
+
+    var streamedResponse = await request.send();
+    var response = await http.Response.fromStream(streamedResponse);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = json.decode(response.body);
+      setState(() {
+        _result = jsonResponse['result'];
+      });
+      if (_result == 'NORMAL') {
+        AwesomeDialog(
+          context: context,
+          dialogType: DialogType.success,
+          animType: AnimType.rightSlide,
+          title: 'Good analysis',
+          desc: 'press Ok to complete your donation check ',
+          btnCancelOnPress: () {
+            Navigator.pop(context);
+          },
+          btnOkOnPress: () async {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DonationView(
+                    HemoglobinLevel: jsonResponse['HEMOGLOBIN'].toDouble(),
+                  ),
+                ));
+
+            setState(() {});
+          },
+        ).show();
+      } else {
+        AwesomeDialog(
+          context: context,
+          dialogType: DialogType.warning,
+          animType: AnimType.rightSlide,
+          title: 'Sorry',
+          desc: ' you can\'t donate press continue to check the reasons',
+          btnCancelOnPress: () {
+            Navigator.pop(context);
+          },
+          btnOkOnPress: () async {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ChatScreen(),
+                ));
+
+            setState(() {});
+          },
+        ).show();
+      }
+    } else {
+      setState(() {
+        _result = 'Failed to upload image';
+      });
+    }
+  }
+
+  bool textScanning = false;
+
+  File? imageFile;
+
+  String scannedText = "";
+  Future<void> getImage(ImageSource imageSource) async {
+    final pickedFile = await picker.pickImage(source: imageSource);
+    setState(() {
+      if (pickedFile != null) {
+        imageFile = File(pickedFile.path);
+        _uploadImage();
+      } else {
+//
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('CBC Test Donation'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.grey[400],
+            // ignore: sort_child_properties_last
+            child: Center(
+              child: imageFile == null
+                  ? const Text('No image selected.')
+                  : Image.file(
+                      File(imageFile!.path),
+                      fit: BoxFit.fill,
+                    ),
+            ),
+
+            width: 200,
+            height: 300,
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.grey[400],
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0)),
+                    ),
+                    onPressed: () {
+                      getImage(ImageSource.gallery);
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 5),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(
+                            Icons.image,
+                            size: 30,
+                          ),
+                          Text(
+                            "Gallery",
+                            style: TextStyle(
+                                fontSize: 13, color: Colors.grey[600]),
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
+              Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.grey[400],
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0)),
+                    ),
+                    onPressed: () {
+                      getImage(ImageSource.camera);
+                    },
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 5, horizontal: 5),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(
+                            Icons.camera_alt,
+                            size: 30,
+                          ),
+                          Text(
+                            "Camera",
+                            style: TextStyle(
+                                fontSize: 13, color: Colors.grey[600]),
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
